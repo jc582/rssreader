3fa04233ed00089656f6d95b69cf8cf92f069e58
James <jc582@optonline.net>
2011-12-22 20:08:44 -0500

    iframe added

--- application.js
+++ application.js
@@ -1,8 +1,23 @@
-  $(function(){
-    $.getJSON('http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=30&callback=?&q=' + encodeURIComponent('http://news.ycombinator.com/rss'), function(data){
-      $.each(data.responseData.feed.entries, function(index, value){
-  			$("#title-list").append('<li><a href="' + value.link + '">' + value.title + '</a></li>');       
-      });
-			$("#title-list").listview("refresh");
-    });
-  });
+$(function(){
+	$.getJSON('http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=30&callback=?&q=' + encodeURIComponent('http://news.ycombinator.com/rss'), function(data){
+		$.each(data.responseData.feed.entries, function(index, value){
+			$("#title-list").append('<li><a href="' + value.link + '"target="external">' + value.title + '</a></li>');       
+		});
+		$("#title-list").listview("refresh");
+	});
+
+	$('a').live("click", function(){
+		$('#title-list').hide();
+		$('#external-iframe').show();
+		$('#external-iframe').css("width", "100%");
+		$('#external-iframe').css("height", "100%");
+		$('.ui-page').css("height", "100%");
+	});
+
+	$('.ui-btn-text:contains("Back")').live("click", function(){
+		$('#title-list').show();
+	});
+
+});
+
+
